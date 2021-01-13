7d2f02c06c18b160d6356ba2e7ddd0fdb6d1c56a
AndreiGarcíaCuadra <andreigarciacuadra@gmail.com>
2021-01-13 02:14:35 +0100

    referee y player adaptados

---
+++ src/referee/plans/ComienzaPartidoPlan.java
@@ -0,0 +1,18 @@
+package referee.plans;
+
+import jadex.runtime.*;
+
+
+public class ComienzaPartidoPlan extends Plan
+{
+	//-------- methods --------
+	
+	/**
+	 *  Plan body.
+	 */
+	public void body()
+	{
+		System.out.println("ya somos suficientes para jugar!!!");
+	}
+
+}
