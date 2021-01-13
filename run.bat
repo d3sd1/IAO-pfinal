3dc207a1d4626105385634e6114b2167bb88e757
AndreiGarcíaCuadra <andreigarciacuadra@gmail.com>
2021-01-13 03:42:42 +0100

    vuelve a reconocer jugadores

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ set file_name=RiskJadex.jar
 del %file_name%
 set classpath=src\lib\Base64.jar;src\lib\GraphLayout.jar;src\lib\bcel.jar;\src\lib\http.jar;src\lib\iiop.jar;src\lib\jade.jar;src\lib\jadeTools.jar;src\lib\jadex_examples.jar;src\lib\jadex_jadeadapter.jar;src\lib\jadex_rt.jar;src\lib\jadex_standalone.jar;src\lib\jadex_tools.jar;src\lib\jhall.jar;src\lib\jibx-bind.jar;src\lib\jibx-extras.jar;src\lib\jibx-run.jar;src\lib\xpp3.jar;src;src\lib\commons-commons\commons-codec-1.3.jar
 
-javac src\tablero\plans\*java
+javac src\partida\plans\*java
 
 pause
 jar cvf %file_name% *
---
+++ src/partida/plans/CompletarJugadoresPlan.java
@@ -0,0 +1,37 @@
+package partida.plans;
+
+import jadex.adapter.fipa.AgentDescription;
+import jadex.adapter.fipa.SearchConstraints;
+import jadex.adapter.fipa.ServiceDescription;
+import jadex.runtime.*;
+
+
+public class CompletarJugadoresPlan extends Plan
+{
+	//-------- methods --------
+	
+	/**
+	 *  Plan body.
+	 */
+	public void body()
+	{
+		ServiceDescription sd = new ServiceDescription();
+		//sd.setType("jugador"); // TODO <- pasar esto como param
+		sd.setName("jugador");
+		AgentDescription adesc = new AgentDescription();
+		adesc.addService(sd);
+		SearchConstraints sc = new SearchConstraints();
+		sc.setMaxResults(-1);
+		IGoal busqueda = createGoal("df_search");
+		busqueda.getParameter("description").setValue(adesc);
+		busqueda.getParameter("constraints").setValue(sc);
+		dispatchSubgoalAndWait(busqueda);
+		AgentDescription[] result =(AgentDescription[])busqueda.getParameterSet("result").getValues();
+		int numjugscasa= result.length;
+		//int numJugadoresEnPartida = Integer.parseInt(getBeliefbase().getBelief("jugador").getFact().toString());
+		System.out.println(numjugscasa+" jugadores casa encontrados, requeridos 6");
+		//getBeliefbase().getBelief("jugadores_en_partida").setFact(++numJugadoresEnPartida);
+		//getBeliefbase().getBelief("agresivo").setFact(numjugscasa);
+	}
+
+}
